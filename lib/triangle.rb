51f8148e2ba459542598512aa841ad3a60097740
joey <pball626@gmail.com>
2021-03-03 20:08:19 -0600

    commit tree a1171816385f3eecc07b59899dd43286639103e0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+
+    attr_accessor :length1, :length2, :length3
+  
+    def initialize
+      @length1 = 15
+      @length2 = 10
+      @length3 = 5
+    end
+    
 end
